d0e4f0173787609222c0c819099a3ca81022a79e
Ann Kh <anna.khrust@gmail.com>
2024-05-11 22:54:42 +0300

    update dropdown

--- dropdown/task.js
+++ dropdown/task.js
@@ -1,12 +1,19 @@
-const dropdownElement = document.getElementsByClassName('dropdown__value')[0]
-dropdownElement.addEventListener('click', function (event) {
-    document.getElementsByClassName('dropdown__list')[0].classList.toggle('dropdown__list_active')
-    const items = document.getElementsByClassName('dropdown__item')
-    for (let i = 0; i < items.length; i++){
-        items[i].addEventListener('click', function (event){
-            event.target.closest('ul').previousElementSibling.innerHTML = event.target.firstElementChild.textContent
-            document.getElementsByClassName('dropdown__list')[0].classList.remove('dropdown__list_active')
-        })
-    }
+const dropValue = document.querySelector(".dropdown__value")
+const dropList = document.querySelector(".dropdown__list")
+const dropLink = document.querySelectorAll(".dropdown__link")
 
-})
+function dropListActive () {
+    dropList.classList.add('dropdown__list_active')
+};
+
+function dropListClose (item) {
+    item.preventDefault();
+    dropValue.textContent = this.textContent;
+    dropList.classList.remove('dropdown__list_active')
+};
+
+dropValue.addEventListener('click', dropListActive);
+
+for (let p = 0; p < dropLink.length; p++) {
+    dropLink[p].addEventListener('click', dropListClose)
+};
